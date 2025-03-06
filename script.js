139865887f30427da3fe0773c24a84d61eee8671
Santhosh1015 <santhoshhh1015@gmail.com>
2025-03-07 00:34:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const name = "Santhosh";
+const nameContainer = document.querySelector(".name");
+
+name.split("").forEach((letter, index) => {
+    const span = document.createElement("span");
+    span.textContent = letter;
+    span.style.animationDelay = `${index * 0.1}s`;
+    nameContainer.appendChild(span);
+});
+
+
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
+function showImage(src) {
+    const modal = document.getElementById("imageModal");
+    const modalImage = document.getElementById("modalImage");
+    modalImage.src = src; // Set the image source
+    modal.classList.add("active"); // Show the modal
+}
+
+function hideImage() {
+    const modal = document.getElementById("imageModal");
+    modal.classList.remove("active"); // Hide the modal
+}
